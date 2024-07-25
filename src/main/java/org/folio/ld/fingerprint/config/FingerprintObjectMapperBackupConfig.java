445e5aa8f0e5f51fb2be3c8aec7b22014976ef81
askhat-abishev <askhat_abishev@epam.com>
2024-07-25 12:59:40 +0500

    MODLD-442: add custom serializer using BeanPostProcessor

--- src/main/java/org/folio/ld/fingerprint/config/FingerprintObjectMapperBackupConfig.java
+++ src/main/java/org/folio/ld/fingerprint/config/FingerprintObjectMapperBackupConfig.java
@@ -3,8 +3,6 @@ package org.folio.ld.fingerprint.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import org.folio.ld.fingerprint.service.FingerprintServiceImpl.FingerprintEntry;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +13,10 @@ public class FingerprintObjectMapperBackupConfig {
   @Bean
   @ConditionalOnMissingBean
   public ObjectMapper objectMapper() {
-    var mapper = new ObjectMapper();
-    return mapper
+    return new ObjectMapper()
       .setSerializationInclusion(JsonInclude.Include.NON_NULL)
       .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-      .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
-      .registerModule(new SimpleModule().addSerializer(FingerprintEntry.class, new FingerprintEntrySerializer()));
+      .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
   }
 
 }
